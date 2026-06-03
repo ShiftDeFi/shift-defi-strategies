53ff1794454c052ddcfd6b8ee79dd99273830f01
shift-developer-2 <shiftdefi-dev-22@proton.me>
2026-06-03 17:29:09 +0300

    feat: morpho vault strategy

---
+++ contracts/interfaces/IMorphoVault.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+interface IMorphoVault {
+    // ---- Structs ----
+
+    struct AutomaticHarvestLocalVars {
+        address morphoVaultCached;
+        address underlyingAssetCached;
+        uint256 rewardTokensLength;
+        uint256 accruedAssetsValue;
+        uint256 accruedVaultTokens;
+        uint256 vaultTokensToTreasury;
+        uint256 lpAmountBefore;
+        uint256 reinvestLpDelta;
+        uint256 feeFromReinvest;
+    }
+
+    struct ManualClaimLocalVars {
+        address morphoVaultCached;
+        address underlyingAssetCached;
+        address strategyContainerCached;
+        uint256 rewardTokensLength;
+        address treasury;
+        uint256 feePct;
+        uint256 accruedAssetsValue;
+        uint256 accruedVaultTokens;
+        uint256 vaultTokensToTreasury;
+        uint256 lpAmountBefore;
+        uint256 reinvestLpDelta;
+        uint256 feeFromReinvest;
+    }
+
+    // ---- Events ----
+
+    event RewardTokensUpdated(address[] rewardTokens);
+
+    // ---- Errors ----
+
+    error RewardTokenMatchesUnderlyingAsset();
+
+    // ---- Functions ----
+
+    /// @notice Sets the reward tokens. Only callabe by role HARVEST_MANAGER_ROLE
+    /// @param _rewardTokens The addresses of the reward tokens
+    function setRewardTokens(address[] memory _rewardTokens) external;
+
+    /// @notice Manually claims the rewards. Only callable by role MERKLE_CLAIMER_ROLE
+    /// @dev Underlying asset claimed via manual claim is instantly reinvested,
+    ///      other reward tokens are reinvested during standard harvest
+    /// @param users The addresses of the users
+    /// @param tokens The addresses of the tokens
+    /// @param amounts The amounts of the tokens
+    /// @param proofs The proofs of the merkle proofs
+    function manualClaim(
+        address[] calldata users,
+        address[] calldata tokens,
+        uint256[] calldata amounts,
+        bytes32[][] calldata proofs
+    ) external;
+}
